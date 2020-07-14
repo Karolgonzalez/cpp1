142a7d80adc3d9c0efa8c7f2a56a05e19a0518f9
Karol Gonzalez <gisellcastro83@gmail.com>
2020-07-13 18:33:54 -0600

    Agregando ejercicio 5 del programa suma

---
+++ Ejercicios/Ejercicio5-Suma/suma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int  main(int argc, char** argv){
+    int a=20, b=30, resultado;
+    
+    resultado  = a + b;
+    
+    cout << "La suma de a + b es: " << resultado;
+
+    return 0;
+
+}
