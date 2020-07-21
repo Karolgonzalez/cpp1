c0f6e110c566f61dd2e477b2980d0339f1a07337
Karol Gonzalez <gisellcastro83@gmail.com>
2020-07-20 23:52:22 -0600

    ejercicio 15 notas completas

--- Ejercicios/Eejrcicio14-Nota/notas.cpp
+++ Ejercicios/Eejrcicio14-Nota/notas.cpp
@@ -6,22 +6,69 @@ int main (int argc, char const *argv[])
     int nota = 0;
      cout << "Ingrese la nota que obtuvo: ";
      cin >> nota;
-    if (nota >= 95 && nota <=100){
-     cout <<(" Obtuviste una A");
+    if (nota >= 96 && nota <=100){
+     cout <<(" Obtuviste una A++");
     }
-    if (nota >=84  && nota <=94){
-     cout <<(" Obtuviste una B");
+    if (nota >=94  && nota <=95){
+     cout <<(" Obtuviste una A+");
     }
-    if (nota >= 75 && nota <=94){
+    if (nota >= 86 && nota <=90){
+     cout <<( "Obtuviste una A-");
+    }
+    if (nota >= 80 && nota <=84){
+     cout <<( "Obtuviste una B+");
+    }
+    if (nota >= 84 && nota <=74){
+     cout <<( "Obtuviste una B+");
+    }
+    if (nota >= 70 && nota <=75){
+     cout <<( "Obtuviste una B");
+    }
+    if (nota >= 65 && nota <=70){
+     cout <<( "Obtuviste una B-");
+    }
+    if (nota >= 60 && nota <=65){
      cout <<( "Obtuviste una C");
     }
-    if (nota >= 65 && nota <=74){
+    if (nota >= 50 && nota <=54){
+     cout <<( "Obtuviste una C-");
+    }
+    if (nota >= 55 && nota <=50){
+     cout <<( "Obtuviste una C-");
+    }
+    if (nota >=45 && nota <=50){
+     cout <<( "Obtuviste una D+");
+    }
+     if (nota >= 45 && nota <=40){
      cout <<( "Obtuviste una D");
     }
-    if (nota >= 60 && nota <=64){
-     cout <<( "Obtuviste una E");
+    if (nota >=44  && nota <=40){
+     cout <<( "Obtuviste una D-");
     }
-    if (nota < 60){
+    if (nota >=35 && nota <=40){
+     cout <<( "Obtuviste una E+");
+    }
+ if (nota >=34  && nota <=30){
+     cout <<( "Obtuviste una E ");
+    }
+    if (nota >=25  && nota <=35){
+     cout <<( "Obtuviste una E+");
+    }
+    if (nota >=20 && nota <=25){
+     cout <<( "Obtuviste una F+");
+    }
+ if (nota >= 15 && nota <=20){
      cout <<( "Obtuviste una F");
+    }
+    if (nota >=10  && nota <=15){
+     cout <<( "Obtuviste una F ");
+    }
+    if (nota >=5 && nota <=10){
+     cout <<( "Obtuviste una F-"); 
+     }
+     if (nota >= 0 && nota <=10){
+     cout <<( "Obtuviste una F--");
+
+    }
     }return 0;
 }
