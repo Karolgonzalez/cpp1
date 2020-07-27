f7ec2477c9b4fc6e243b42c04904a951b5a7d421
Karol Gonzalez <gisellcastro83@gmail.com>
2020-07-27 00:00:08 -0600

    Aregando  ejercicio 18 menu

--- Ejercicios/Ejercicio18-Menu/menu.cpp
+++ Ejercicios/Ejercicio18-Menu/menu.cpp
@@ -17,12 +17,12 @@ int main(int argc, char**argv) {
       cin >> opcion;
       
       if (opcion == 1){
-          system('cls');
+          system("cls");
           cout << "Estas en el menu de cafe y granitas"<< endl;
           system ("pause");
       }
      if  (opcion == 2){
-          system('cls');
+          system("cls");
           cout << "Estas en el menu de reposteria"<< endl;
           system ("pause");
       }
