965fc405a983cc95f79a05470d606d544f0783f2
Karol Gonzalez <gisellcastro83@gmail.com>
2020-08-03 23:00:16 -0600

    Agregando ejercicios 40, 41, 42

---
+++ Ejercicios/Ejercicio42-Punto-de-Venta-parte-III/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+usig namespace std;
+
+int main( int argc, char const *argv[])
+{
+  int opcion = 0;
+while (true)
+{
+    system ("cls");
+
+    cout <<"****";
+    cout <<"menu";
+    cout <<"****";
+    cout <<endl;
+
+    cout << "1 - Bebidas calientes" << endl;
+    cout << "2 - Bebidas Frias" << endl;
+    cout << "3 - Reposteria"<<endl;
+    cout << "0 - Salir" << endl;
+    
+    
+    cin >> opcion;
+
+    if (opcion == 0) 
+      {
+          break;
+       }
+}
+return 0;
+}
+ 
---
+++ Ejercicios/Ejercicio42-Punto-de-Venta-parte-III/menu.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+externsl void productos(int opcion)
+
+
+void menu ()
+
+{  
+      int opcion = 0;
+      while (true)
+      {
+            system("cls");
+
+            cout <<"****";
+            cout <<"menu";
+            cout <<"****";
+            cout<< endl;
+            cout << endl;
+            cout<< endl;
+            
+            
+            cout <<"1 - BEBIDAS CALIENTES "<< endl;
+            cout <<"2 - BEBIDAS FRIAS" << endl;
+            cout <<"3 - REPOSTERIA" << endl;
+            cout <<"0 - SALIR" << endl;
+            
+            cin >> opcion;
+
+            if  (opcion == o)
+            {
+
+            break;
+            }
+            productos (opcion);
+      }
+
+} 
+
