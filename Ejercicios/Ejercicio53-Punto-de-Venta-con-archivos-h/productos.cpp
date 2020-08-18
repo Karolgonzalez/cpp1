538047b0b75d1db10616dda38e48174af326f845
Karol Gonzalez <gisellcastro83@gmail.com>
2020-08-17 21:18:56 -0600

    aregando ejercicios

--- Ejercicios/Ejercicio53-Punto-de-Venta-con-archivos-h/productos.cpp
+++ Ejercicios/Ejercicio53-Punto-de-Venta-con-archivos-h/productos.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
+#include "productos.h"
 
 using namespace std;
-
-extern void agregarProducto(string descripcion, int cantidad, double precio);
 
 void productos(int opcion)
 {
@@ -27,13 +26,13 @@ void productos(int opcion)
         switch (opcionProducto)
         {
         case 1:
-            agregarProducto("1 Expreso - L 20.00", 1, 20);
+            agregarProducto("1 Expresso  - L20 .00", 1, 20 );
             break;
         case 2:
             agregarProducto("1 Chocolate - L 45.00", 1, 45);
             break;
         case 3:
-            agregarProducto("1 Cafe Dalgona - L 60.00", 1, 60);
+            agregarProducto("1 Cafe Dalgona - L60 .00", 1, 60);
             break;
 
         default:
@@ -54,9 +53,9 @@ void productos(int opcion)
     {
         cout << "BEBIDAS FRIAS" << endl;
         cout << "*************" << endl;
-        cout << "1 - Frappe de Fresa" << endl;
-        cout << "2 - Malteada Mora azul" << endl;
-        cout << "3 - Moccaccino Suprime" << endl;
+        cout << "1 Frappe de fresa- " << endl;
+        cout << "2 Malteada Mora azul - " << endl;
+        cout << "3 Moccaccino Suprime - " << endl;
         cout << endl;
 
         cout << "Ingrese una opcion: ";
@@ -65,13 +64,13 @@ void productos(int opcion)
         switch (opcionProducto)
         {
         case 1:
-            agregarProducto("1 Frappe de Fresa - L 55.00", 1, 55);
+            agregarProducto("1 Frappe de fresa  - L 55.00", 1, 55 );
             break;
         case 2:
-            agregarProducto("1 Malteada Mora azul - L 50.00", 1, 50);
+            agregarProducto("1 Malteada Mora azul  - L50 .00", 1, 50 );
             break;
         case 3:
-            agregarProducto("1 Moccaccino Suprime - L 67.00", 1, 67);
+            agregarProducto("1 Moccaccino Suprime - L 67.00", 1,67 );
             break;
         
         default:
@@ -92,9 +91,9 @@ void productos(int opcion)
     {
         cout << "REPOSTERIA" << endl;
         cout << "**********" << endl;
-        cout << "1 - Chocochips" << endl;
-        cout << "2 - Pay" << endl;
-        cout << "3 - Sandwich" << endl;
+        cout << "1 Chocochips - " << endl;
+        cout << "2 Pay - " << endl;
+        cout << "3 Sandwich- " << endl;
         cout << endl;
 
         cout << "Ingrese una opcion: ";
@@ -103,13 +102,13 @@ void productos(int opcion)
         switch (opcionProducto)
         {
         case 1:
-            agregarProducto("1 Chocochips - L 18.00", 1, 18);
+            agregarProducto("1 Chocochips  - L 18.00", 1, 18 );
             break;
         case 2:
-            agregarProducto("1 Pay - L 30.00", 1, 30);
+            agregarProducto("1 Pay - L30 .00", 1, 30 );
             break;
         case 3:
-            agregarProducto("1 Sandwich - L 20.00", 1, 20);
+            agregarProducto("1  Sandwich- L 20 .00", 1, 20 );
             break;
         
         default:
